029e9c4ef3ccc63dcea48db6fa02aeff5d188167
frankkulak <fs.kulak@gmail.com>
2024-04-07 17:51:29 -0400

    Update home schema and build script.

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -19,17 +19,24 @@ function srcToDstPath(srcPath) {
 
 /**
  * @param {string} filepath
- * @param {(json: object) => void} preprocess
- * @param {(content: string) => string} postprocess
+ * @param {object} options
+ * @param {(content: string) => string} options.modifyInputContent
+ * @param {(json: object) => void} options.processObject
+ * @param {(content: string) => string} options.modifyOutputContent
  */
-function minifyJson(filepath, preprocess, postprocess) {
+function minifyJson(filepath, options) {
   const dstPath = srcToDstPath(filepath);
-  const json = JSON.parse(fs.readFileSync(filepath).toString());
+  const rawContent = fs.readFileSync(filepath).toString();
+  const contentToParse = options?.modifyInputContent
+    ? options.modifyInputContent(rawContent)
+    : rawContent;
+  const json = JSON.parse(contentToParse);
   delete json.$schema;
-  preprocess?.(json);
-  let content = JSON.stringify(json);
-  if (postprocess) content = postprocess(content);
-  fs.writeFileSync(dstPath, content);
+  options?.processObject?.(json);
+  let outputContent = JSON.stringify(json);
+  if (options?.modifyOutputContent)
+    outputContent = options.modifyOutputContent(content);
+  fs.writeFileSync(dstPath, outputContent);
 }
 
 //#endregion
@@ -37,24 +44,41 @@ function minifyJson(filepath, preprocess, postprocess) {
 //#region Build
 
 const variables = {
-  numPublishedProjects: 0, // count maintained/archived projects in projects.json
+  numProjects: 0, // all
+  numAvailableProjects: 0, // maintained/archived only
+  numWebsiteProjects: 0, // tag=website
+  numModToolProjects: 0, // tag=ts4-tool
+  numModProjects: 0, // tag=ts4-mod
 };
 
-minifyJson(path.join(SRC_DIR, "projects.json"), (json) => {
-  json.sections.forEach(({ projects }) => {
-    projects.forEach((project) => {
-      if (project.status === "maintained" || project.status === "archived") {
-        variables.numPublishedProjects++;
-      }
+minifyJson(path.join(SRC_DIR, "projects.json"), {
+  processObject(json) {
+    json.sections.forEach(({ projects }) => {
+      variables.numProjects += projects.length;
+      projects.forEach(({ status, tags }) => {
+        if (status === "maintained" || status === "archived")
+          variables.numAvailableProjects++;
+        if (tags.includes("website")) variables.numWebsiteProjects++;
+        if (tags.includes("ts4-tool")) variables.numModToolProjects++;
+        if (tags.includes("ts4-mod")) variables.numModProjects++;
+      });
     });
-  });
+  },
 });
 
-minifyJson(path.join(SRC_DIR, "home.json"), null, (content) =>
-  content.replace(
-    /{{numPublishedProjects}}/g,
-    variables.numPublishedProjects.toString()
-  )
-);
+minifyJson(path.join(SRC_DIR, "home.json"), {
+  modifyInputContent(content) {
+    for (const [key, value] of Object.entries(variables)) {
+      content = content.replace(new RegExp(`{{${key}}}`, "g"), value);
+    }
+
+    return content;
+  },
+  processObject(json) {
+    json.projects.dashboard.cells.sort((a, b) => {
+      return parseInt(b.count) - parseInt(a.count);
+    });
+  },
+});
 
 //#endregion
