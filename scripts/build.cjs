9fb97c52410087bd60ac82e067dfc4e28916e2eb
frankkulak <fs.kulak@gmail.com>
2024-04-07 23:10:28 -0400

    Add enums file.

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -18,13 +18,14 @@ function srcToDstPath(srcPath) {
 }
 
 /**
- * @param {string} filepath
+ * @param {string} jsonName
  * @param {object} options
  * @param {(content: string) => string} options.modifyInputContent
  * @param {(json: object) => void} options.processObject
  * @param {(content: string) => string} options.modifyOutputContent
  */
-function minifyJson(filepath, options) {
+function minifyJson(jsonName, options) {
+  const filepath = path.join(SRC_DIR, `${jsonName}.json`);
   const dstPath = srcToDstPath(filepath);
   const rawContent = fs.readFileSync(filepath).toString();
   const contentToParse = options?.modifyInputContent
@@ -51,11 +52,36 @@ const variables = {
   numModProjects: 0, // tag=ts4-mod
 };
 
-minifyJson(path.join(SRC_DIR, "projects.json"), {
+const enums = {
+  tags: undefined, // string set
+  skills: undefined, // string set
+};
+
+minifyJson("enums", {
+  processObject(json) {
+    // register enums
+    enums.tags = new Set(Object.keys(json.tags));
+    enums.skills = new Set(Object.keys(json.skills));
+  },
+});
+
+minifyJson("projects", {
   processObject(json) {
     json.sections.forEach(({ projects }) => {
+      // count variables
       variables.numProjects += projects.length;
-      projects.forEach(({ status, tags }) => {
+      projects.forEach(({ name, status, tags, techStack, oldTechStack }) => {
+        // ensure tags / tech stack contain valid values
+        const ensureValid = (list, kind) => {
+          const validSet = enums[kind];
+          const found = list.find((item) => !validSet.has(item));
+          if (found) throw `Project '${name}' has ${kind} '${found}'`;
+        };
+        ensureValid(tags, "tags");
+        ensureValid(techStack, "skills");
+        if (oldTechStack) ensureValid(oldTechStack, "skills");
+
+        // count variables
         if (status === "maintained" || status === "archived")
           variables.numAvailableProjects++;
         if (tags.includes("website")) variables.numWebsiteProjects++;
@@ -66,15 +92,15 @@ minifyJson(path.join(SRC_DIR, "projects.json"), {
   },
 });
 
-minifyJson(path.join(SRC_DIR, "home.json"), {
+minifyJson("home", {
   modifyInputContent(content) {
-    for (const [key, value] of Object.entries(variables)) {
+    // replace variables
+    for (const [key, value] of Object.entries(variables))
       content = content.replace(new RegExp(`{{${key}}}`, "g"), value);
-    }
-
     return content;
   },
   processObject(json) {
+    // sort projects dashboard by count
     json.projects.dashboard.cells.sort((a, b) => {
       return parseInt(b.count) - parseInt(a.count);
     });
